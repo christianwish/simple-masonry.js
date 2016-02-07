5b8c34c79e17f52f3786da29dfe1d05fa20f6f37
Christian Heyn <chris.heyn.dd@gmail.com>
2016-02-07 23:07:47 +0100

    module.exports

--- dist/simple-masonry-compiled.js
+++ dist/simple-masonry-compiled.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -511,3 +513,7 @@ var SimpleMasonry = function () {
 
     return SimpleMasonryInner;
 }();
+
+if (module && (typeof module === 'undefined' ? 'undefined' : _typeof(module)) === 'object') {
+    module.exports = SimpleMasonry;
+}
